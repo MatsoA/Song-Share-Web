ac24edd782d0f4b4639b53d8a05dac397e3774e6
MatsoA <19639672+MatsoA@users.noreply.github.com>
2023-03-18 18:13:34 -0400

    Added helper text for friend search

--- song-share-web/src/Components/ManageFriendPage.js
+++ song-share-web/src/Components/ManageFriendPage.js
@@ -1,14 +1,14 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { collection, doc, addDoc, setDoc, getDoc, getDocs, query, where } from "firebase/firestore"
 import { firebaseApp, authProvider, database } from "./firebaseConfig"
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 import { Stack, TextField, Button, Box, Item, Paper, Divider } from '@mui/material';
-import {styled} from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import Banner from './banner';
 import PendingFriendList from './PendingFriendList'
 import ActiveFriendList from './ActiveFriendsList'
 
-export default function ManageFriendPage({userDetails, setUserDetails}) {
+export default function ManageFriendPage({ userDetails, setUserDetails }) {
 
     //hook for input field
     const [entry, setEntry] = useState("")
@@ -17,13 +17,15 @@ export default function ManageFriendPage({userDetails, setUserDetails}) {
     const [missingEntry, setMissingEntry] = useState(false);
     const [unfoundUsername, setUnfoundUsername] = useState(false);
 
+    const [requestStatus, setRequestStatus] = useState("");
+
     //start search for friend upon submission
     const handleSubmit = e => {
 
         e.preventDefault();
 
         //raise flag if no entry is typed
-        if (entry.length == 0) {
+        if (entry.length === 0) {
             setMissingEntry(true);
         }
 
@@ -31,27 +33,40 @@ export default function ManageFriendPage({userDetails, setUserDetails}) {
             //query for how to look for friend (via email)
             const friendSearch = query(collection(database, "userList"), where("email", "==", entry));
 
-            
+
             //if valid entry, excecute query and look for friend
-            (async function() {
+            (async function () {
                 const querySnapshot = await getDocs(friendSearch);
 
                 //if query is empty, flag will remain true
                 setUnfoundUsername(true);
+                setRequestStatus("Unable to find user for email");
 
                 //send request to friend found
                 querySnapshot.forEach((document) => {
                     setUnfoundUsername(false);
 
                     //TODO: only add as pending if not already active
-                    
-                    (async function() {
-                        await setDoc(doc(database, "userList", document.data().uID, "friendList", userDetails.uid), {
-                            friendStatus: "pending"
-                        })
+                    console.log(document.data());
+
+                    (async function () {
+                        const friendTest = await getDoc(doc(database, "userList", document.data().uID, "friendList", userDetails.uid))
+
+                        if (!friendTest.exists()) {
+                            await setDoc(doc(database, "userList", document.data().uID, "friendList", userDetails.uid), {
+                                friendStatus: "pending"
+                            })
+
+                            setRequestStatus("Request Sent")
+                        } else {
+                            setRequestStatus("Already Friends")
+                        }
                     })();
 
                 })
+
+
+
             })();
         }
 
@@ -67,36 +82,38 @@ export default function ManageFriendPage({userDetails, setUserDetails}) {
         //reset error trackers since new input
         setMissingEntry(false);
         setUnfoundUsername(false);
+        setRequestStatus("");
     }
 
 
     return (
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, bgcolor: 'background.default', pl: 30, pt: 5 }}>
-                <form noValidate autoComplete='off' onSubmit={handleSubmit}>
-                    <TextField
-                        label="Search User"
-                        error = {(missingEntry) || (unfoundUsername)}
-                        variant="outlined"
-                        value={entry}
-                        onChange={(e) => {
-                            handleTextInputChange(e)
-                        }}
-                    />
-                    <Button type="submit" variant="contained">Search</Button>
-                </form>
-                
-                <Divider />
+        <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', pl: 30, pt: 5 }}>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                <TextField
+                    helperText={requestStatus}
+                    label="Search User"
+                    error={(missingEntry) || (unfoundUsername)}
+                    variant="outlined"
+                    value={entry}
+                    onChange={(e) => {
+                        handleTextInputChange(e)
+                    }}
+                />
+                <Button type="submit" variant="contained">Search</Button>
+            </form>
 
-                <text>Incoming Friend Requests</text>
-                <PendingFriendList userDetails = {userDetails} />
+            <Divider />
 
-                <Divider />
+            <text>Incoming Friend Requests</text>
+            <PendingFriendList userDetails={userDetails} />
 
-                <text>Friends</text>
-                <ActiveFriendList userDetails = {userDetails} />
+            <Divider />
 
-            </Box>
+            <text>Friends</text>
+            <ActiveFriendList userDetails={userDetails} />
+
+        </Box>
     )
 }
